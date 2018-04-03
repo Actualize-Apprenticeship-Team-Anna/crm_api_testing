8c6ffe5e784a6598ba412102e0b825ea5d4f9ba4
Mark Custard <markc1772@gmail.com>
2018-04-02 19:01:41 -0500

    remove blank space

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,9 +25,6 @@ SimpleCov.start do
 
 end
 RSpec.configure do |config|
-
-
-
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
