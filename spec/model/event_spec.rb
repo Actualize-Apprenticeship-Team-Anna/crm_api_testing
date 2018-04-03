8749342cdcca476349ec691976f203772660f965
Mark Custard <markc1772@gmail.com>
2018-04-02 19:06:00 -0500

    name consistency

--- spec/model/event_spec.rb
+++ spec/model/event_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Event, :type => :model do
   before(:all) do
-    @event1 = create(:event1)
+    @event = create(:event)
   end
 
   it "is valid with valid attributes" do
-    expect(@event1).to be_valid
+    expect(@event).to be_valid
   end
 end
