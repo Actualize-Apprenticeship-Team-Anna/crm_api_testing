3d7843590e8b40a72b57508658ea15e3dcc1bbde
Mark Custard <markc1772@gmail.com>
2018-04-02 19:58:44 -0500

    trying to create an admin with an invalid email

--- spec/model/admin_spec.rb
+++ spec/model/admin_spec.rb
@@ -3,10 +3,14 @@ require 'rails_helper'
 RSpec.describe Admin, :type => :model do
   before(:all) do
     @admin1 = create(:admin)
-
   end
 
   it "is valid with valid attributes" do
     expect(@admin1).to be_valid
   end
+
+  it "is invalid with invalid attributes" do
+    expect{ create(:admin, email: "5")}.to raise_error (ActiveRecord::RecordInvalid)
+  end
+
 end
